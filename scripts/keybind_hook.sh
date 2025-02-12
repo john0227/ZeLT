f3acdb59648e851ff2f1787539ba8de0a59925a5
명신민 <minjohn0227@gmail.com>
2025-02-12 08:18:38 +0800

    Create hook to refresh keybinds for each mode

--- scripts/keybind_hook.sh
+++ scripts/keybind_hook.sh
@@ -4,8 +4,14 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT="$(dirname $CWD)"
 FILE="${ROOT}/hints/&1.txt"
 
+if [[ ! -d "${ROOT}/hints" ]]; then
+    mkdir "${ROOT}/hints"
+fi
+
 for mode in "pane" "window" "session"; do
     _FILE=$(sed "s/&1/$mode/g" <<< $FILE)
+    touch $_FILE
+
     # escape non-alphanumeric keybinds (that are not already escaped)
     # tmux escapes '\' by default
     tmux list-keys -T "${mode}-mode" | sed "s/.*${mode}-mode //g" | sed -E 's/^[^0-9a-zA-Z\]/\\&/g' | sort -s -b -k2,2 > $_FILE
--- zelt.tmux
+++ zelt.tmux
@@ -2,5 +2,12 @@
 
 CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+tmux source "${CWD}/options.conf"
 tmux source "${CWD}/keybinds.conf"
+
+# Create hook to collect keybinds whenever new session is created
+tmux set-hook -ga session-created "run-shell ${CWD}/scripts/keybind_hook.sh"
+# Create a keybind to allow users to run hook whenever they want
+HOOK_KEY=$(tmux show-options -gqv @zelt_keybind_hook_key)
+tmux bind -T prefix "$HOOK_KEY" "run-shell ${CWD}/scripts/keybind_hook.sh"
 
